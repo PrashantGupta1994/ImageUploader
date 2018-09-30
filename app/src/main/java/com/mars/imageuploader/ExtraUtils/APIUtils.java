13193ff5ef15d212c334cd01e31d52562845df88
Prashant Gupta <pkgeffects@gmail.com>
2018-09-30 17:06:50 +0530

    3
    Cloud ++

--- app/src/main/java/com/mars/imageuploader/ExtraUtils/APIUtils.java
+++ app/src/main/java/com/mars/imageuploader/ExtraUtils/APIUtils.java
@@ -6,13 +6,13 @@ public class APIUtils {
     public static final String S_NAME = "MarsPlay";
     public static final String DO_NOT_SHOW_AGAIN = "idk";
 
-    public static final String IMAGE_TAG = "parle-g";
+    public static final String IMAGE_TAG = S_NAME;
     public static final String JSON = ".json";
     public static final String JPEG = ".jpg";
 
-    public static final String CLOUD_NAME = "dbpqx9aqg";
-    public static final String CLOUD_KEY = "838268216839538";
-    public static final String CLOUD_SECRET = "6dRgp9bJa-lw_MjGBxy6FqdcFc4";
+    public static final String CLOUD_NAME = "test-cloud-007";
+    public static final String CLOUD_KEY = "224597545485481";
+    public static final String CLOUD_SECRET = "1y_3I_yHTPsMtFNUVRMW0woY7b8";
     public static final String URL_BASE = "http://res.cloudinary.com/"+ CLOUD_NAME +"/image/";
 
     public static final String URL_DOWNLOAD_IMAGE_ID = URL_BASE + "list/" + IMAGE_TAG + JSON;
